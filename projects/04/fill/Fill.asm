badab727984cf539fd759f60332cd6002a11b9fd
rustberry <34592958+rustberry@users.noreply.github.com>
2019-03-08 16:05:53 +0800

    project 04, machine code and assembly

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,80 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    @flag
+    M=0
+    @addrW
+    M=0
+    @addrB
+    M=0
+    @24576
+    D=A
+    @MAX_SCR
+    M=D
+
+(RESET)
+    @SCREEN // 16384 -- 24576
+    D=A
+    @addrW
+    M=D
+    @addrB
+    M=D
+    @flag
+    D=M
+    @FILL
+    D;JGT
+    @CLEAR
+    D;JEQ
+
+(FILL)
+    @flag   // Enter FILL loop
+    M=1
+
+    @addrB  // If enters @KBD
+    D=M
+    @MAX_SCR
+    D=M-D
+    @RESET
+    D;JEQ   //Reset start point
+
+    @addrB  // Fill screen
+    D=M     // The value of @addrB is the addr of target pixel
+    A=D     
+    M=-1    // M=1 won't work, since we're operating 16-bit-ly
+            // This only results arrays of 0000 0000 0000 0001
+    @addrB
+    M=M+1
+
+    @KBD    // If no keys down
+    D=M
+    @CLEAR
+    D;JEQ
+
+    @FILL
+    0;JMP
+
+(CLEAR)
+    @flag
+    M=0
+
+    @addrW  // If enters @KBD
+    D=M
+    @MAX_SCR
+    D=M-D
+    @RESET
+    D;JEQ   //Reset start point
+
+    @addrW  // Clear screen
+    D=M
+    A=D
+    M=0     // Addr in A
+    @addrW
+    M=M+1
+    
+    @KBD    //Could be abstracted
+    D=M
+    @FILL
+    D;JGT
+
+    @CLEAR
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,36 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @2      // Initialize sum as 0
+    M=0     
+    @0      // Ensure R0 and R1 != 0   
+    D=M
+    @END
+    D;JEQ
+    @1
+    D=M
+    @END
+    D;JEQ
+
+    @0      // sum = R0
+    D=M
+    @2
+    M=D
+
+(LOOP)
+    @1      // If R1 == 0
+    M=M-1
+    D=M
+    @END    // Jmp to END
+    D;JEQ
+
+    @0      // R2 += R0
+    D=M
+    @2
+    M=M+D
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
